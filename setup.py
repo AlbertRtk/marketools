4bbd0922fd737db12ca828a10c1524b9aac8fd1f
Albert <ratajczakalbert@gmail.com>
2020-11-29 13:56:51 +0100

    setup.py up version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="marketools",
-    version="0.0.8",
+    version="0.1.0",
     author="Albert Ratajczak",
     author_email="ratajczakalbert@gmail.com",
     description="Tools for stock market analysis.",
