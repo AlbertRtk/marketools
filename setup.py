7f244411929a741a11957383733424e6f50cd205
Albert <ratajczakalbert@gmail.com>
2020-11-29 09:56:37 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="setuptool",
+    name="marketools",
     version="0.0.1",
     author="Albert Ratajczak",
     author_email="ratajczakalbert@gmail.com",
