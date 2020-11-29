8708e49f17245b961c46444a748a1b20978dd219
Albert <ratajczakalbert@gmail.com>
2020-11-29 09:35:38 +0100

    adding setup.py and README.md update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# marketools
+marketools
+==========
+
 Tools for stock market analysis.
+
+[Github](https://github.com/AlbertRtk/marketools)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="setuptool",
+    version="0.0.1",
+    author="Albert Ratajczak",
+    author_email="ratajczakalbert@gmail.com",
+    description="Tools for stock market analysis.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlbertRtk/marketools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
