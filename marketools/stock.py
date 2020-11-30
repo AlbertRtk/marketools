375c3d20ff30375d313c189454d3fc4e2d20c600
Albert <ratajczakalbert@gmail.com>
2020-11-30 16:09:50 +0100

    doc for stock.py

--- marketools/stock.py
+++ marketools/stock.py
@@ -6,7 +6,31 @@ import os
 
 
 class Stock:
-    def __init__(self, ticker):
+    """
+    Class representing a stock with given ticker.
+
+    Attributes
+    ----------
+    ticker : str
+        ticker of a stock; country code needs to be append after a dot for not
+        Polish stocks, e.g, 'AAPL.US'
+    ohlc : pandas.DataFrame
+        DataFrame with OHLC prices (open-high-low-close), and volume
+    fundamentals : dict
+        dictionary with available fundamental information
+    """
+
+    def __init__(self, ticker: str):
+        """
+        Constructs stock object.
+
+        Parameters
+        ----------
+        ticker : str
+            ticker of a stock; country code needs to be append after a dot for 
+            not Polish stocks, e.g, 'AAPL.US'
+        """
+
         self.ticker = ticker
         self._ohlc = StockQuotes(ticker)
         self._fundamentals = Fundamentals(ticker)
@@ -18,37 +42,45 @@ class Stock:
 
     @property
     def ohlc(self):
+        """
+        Returns DataFrame with OHLC prices (open-high-low-close), and volume.
+        """
         return self._ohlc.data
 
     @property
     def last_ohlc(self):
+        """
+        Returns the most recent OHLC prices (open-high-low-close), and volume.
+        """
         return self.ohlc.iloc[-1]
 
     @property
     def date(self):
-        """ Returns date of last price """
+        """Returns date of last price."""
         output = self.last_ohlc.name
         output = output.strftime('%Y-%m-%d')
         return output
 
     @property
     def price(self):
-        """ Returns last price """
+        """Returns last price."""
         return self.last_ohlc['Close']
 
     @property
     def volume(self):
-        """ Returns last session volume"""
+        """Returns last session volume."""
         return self.last_ohlc['Volume']
 
     @property
     def fundamentals(self):
+        """Returns fundamental information, if available."""
         if not bool(self._fundamentals):
             self._fundamentals.get_fundamentals()
         return self._fundamentals
 
     @property
     def eps(self):
+        """Returns earning per share, if available."""
         output = self.fundamentals.get('EPS')
         if output is not None:
             output = float(output)
@@ -56,6 +88,7 @@ class Stock:
 
     @property
     def pe(self):
+        """Returns price–earnings ratio, if available."""
         output = self.fundamentals.get('P/E')
         if output is not None:
             output = float(output)
@@ -63,6 +96,7 @@ class Stock:
 
     @property
     def pbv(self):
+        """Returns price–to-book ratio, if available."""
         output = self.fundamentals.get('P/BV')
         if output is not None:
             output = float(output)
@@ -70,6 +104,7 @@ class Stock:
 
     @property
     def dividend_yield(self):
+        """Returns dividend yield, if available."""
         output = self.fundamentals.get('Dividend yield')
         if output is not None:
             output = output[0:-1]
@@ -78,10 +113,24 @@ class Stock:
 
     @property
     def stooq_plot_link(self):
+        """Returns link Stooq page with HTML chart with OHLC data."""
         link = f'https://stooq.pl/q/a2/?s={self.ticker}'
         return link
 
-    def mean_volume(self, over_last):
+    def mean_volume(self, over_last: int):
+        """
+        Returns mean volume over given number of the most recent sessions.
+
+        Parameters
+        ----------
+        over_last : int
+            number of recent stock market sessions the average should be 
+            calculated over
+
+        Returns
+        -------
+        float
+        """
         volume = self.ohlc.tail(over_last)['Volume']
         output = volume.mean()
         return output
