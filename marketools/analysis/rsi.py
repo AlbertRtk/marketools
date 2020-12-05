8e253f95a31551bb74374c47a99303f19470dee0
Albert <ratajczakalbert@gmail.com>
2020-12-05 11:53:07 +0100

    rsi fix

--- marketools/analysis/rsi.py
+++ marketools/analysis/rsi.py
@@ -25,7 +25,7 @@ def rsi(prices: pd.DataFrame, window: int = 14):
     price_changes['Down'] = price_prev - price_now  # downward changes
     price_changes[price_changes < 0] = 0
 
-    smma = price_changes.ewm(span=window, adjust=False).mean()  # smoothed moving averages
+    smma = price_changes.ewm(alpha=1/window, adjust=False).mean()  # smoothed moving averages
     rs = smma['Up'] / smma['Down']
 
     output_rsi = -100 / (rs+1)
