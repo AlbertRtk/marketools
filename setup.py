5c9e81f8331ce52f8edad48b2c6aff7ef97ce34e
Albert <ratajczakalbert@gmail.com>
2020-12-06 09:56:38 +0100

    licence update in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD-3-Clause License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
