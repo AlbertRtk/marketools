cb312d3b6fa1d244654a0b642847b951ebe702b6
Albert <ratajczakalbert@gmail.com>
2020-12-15 14:31:03 +0100

    adding Simulator class

--- marketools/__init__.py
+++ marketools/__init__.py
@@ -1,9 +1,9 @@
 from marketools.stock import *
 from marketools.wallet import Wallet
-from marketools.simulator import simulator
+from marketools.simulator import simulator, Simulator
 from marketools.analysis import *
 
 
-__pdoc__ = {}
+__pdoc__ = dict()
 __pdoc__['extra_print'] = False
 __pdoc__['wallet'] = False
