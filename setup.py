64e158704a52fe08da6cd872dcfe2211b4a4f707
Albert <ratajczakalbert@gmail.com>
2021-02-26 11:01:42 +0100

    pre-release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="marketools",
-    version="1.0.0",
+    version="1.0.1",
     maintainer="Albert Ratajczak",
     maintainer_email="ratajczakalbert@gmail.com",
     description="Tools for stock market analysis.",
