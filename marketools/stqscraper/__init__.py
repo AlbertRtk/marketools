3edacb6a80794a0298539d3094dc0264fc8bdb48
Albert <ratajczakalbert@gmail.com>
2021-02-26 10:55:03 +0100

    marketools storage update - comments

--- marketools/stqscraper/__init__.py
+++ marketools/stqscraper/__init__.py
@@ -6,6 +6,10 @@ DWL_DATA_DIR = os.path.join(os.path.expanduser('~'), '.marketools_data')
 
 
 def store_data():
+    """
+    By default scraped/downloaded data are not stored.
+    This function enables data storage on local machine.
+    """
     global STORE_DWL_DATA
     STORE_DWL_DATA = True
     if not os.path.exists(DWL_DATA_DIR):
@@ -13,8 +17,10 @@ def store_data():
 
 
 def get_storage_status():
+    """Returns True is data storage is active."""
     return STORE_DWL_DATA
 
 
 def get_storage_dir():
+    """Returns storage directory."""
     return DWL_DATA_DIR
