5138e2b7e81066c19fdb249f579f6628f1d4be74
Albert <ratajczakalbert@gmail.com>
2021-04-01 15:48:31 +0200

    move select_stocks_with_increased_volume

--- marketools/analysis/volume.py
+++ marketools/analysis/volume.py
@@ -1,8 +1,6 @@
 
 
-def mean_volume_on_date(volume_data, 
-                        day, 
-                        window=90):
+def mean_volume_on_date(volume_data, day, window=90):
     """
     Returns mean volume over given number of sessions before given date 
     (inclusively).
@@ -26,3 +24,33 @@ def mean_volume_on_date(volume_data,
     output = volume.mean()
     
     return output
+
+
+def select_stocks_with_increased_volume(stocks_dict: dict, long: int = 90, factor: float = 3.3) -> dict:
+
+    """
+    Returns dictionary with stocks (tickers as keys, Stock as values) that volume increased over a given factor compared
+    to long average of volume (90 days by default).
+
+    Parameters
+    ----------
+    stocks_dict : dict
+        dictionary with tickers as keys and Stock as values
+    long : int
+        average window
+    factor : float
+        factor for minimum increase of volume
+
+    Returns
+    -------
+    dict
+    """
+    selected = dict()
+
+    for tck in stocks_dict.keys():
+        stock = stocks_dict[tck]
+        long_mean = stock.mean_volume(long)
+        if (stock.volume/long_mean) > factor:
+            selected[tck] = stock
+
+    return selected
